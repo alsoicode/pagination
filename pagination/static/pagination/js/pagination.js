8bf302a949b3a0e5db4c97e8e59c9eea25c65fc8
Brandon Taylor <brandon@iambrandontaylor.com>
2014-12-14 13:15:38 -0500

    Added amd support to pagination js instead of using a separate file.

--- pagination/static/pagination/js/pagination.js
+++ pagination/static/pagination/js/pagination.js
@@ -1,8 +1,14 @@
-(function($) {
+!function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define('jquery', factory);
+    } else {
+        factory(root.jQuery);
+    }
+}(this, function($) {
     $(document).ready(function() {
         // Handler for items per page form
         $('.items-per-page, .paginator').change(function(e){
             $(e.target).parents('form:first').submit();
         });
-    });  
-})(jQuery);
+    });
+});
